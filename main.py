37687f099b975f230c7d8bfe14a960f21ede8170
RainelC <rainelmartinez13@gmail.com>
2025-02-21 16:32:00 -0400

    unicorn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response
 from base64_conv import b64_to_byte
+import uvicorn
 from db import *
 import random
 
@@ -10,6 +11,10 @@ async def root():
     pictures = list(map(lambda x : fs.get(file_id=x['image']['file_id']).read() , list(db.pictures.find({}))))
     image_byte = b64_to_byte(random.choice(pictures))
     return Response(content=image_byte, media_type="image/jpg")
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 db, fs, collection = conexion_db()
